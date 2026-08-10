05d4c3c3dc94628a8d92737916e59c8172666a5f
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-10 21:48:16 +0700

    upload: scripts/nginx.sh

---
+++ scripts/nginx.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+setup_nginx() {
+  [ "$ENABLE_NGINX" != "true" ] && return 0
+
+  local app_port="${APP_PORT:-3000}"
+  local listen_port="${NGINX_PORT:-8080}"
+  local conf_path="/etc/nginx/sites-enabled/app.conf"
+
+  cat > "$conf_path" << NGINXEOF
+server {
+    listen ${listen_port};
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:${app_port};
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+NGINXEOF
+
+  nginx -t >/tmp/nginx-test.log 2>&1
+  if [ $? -eq 0 ]; then
+    nginx >/tmp/nginx.log 2>&1
+  else
+    echo -e "\033[1;31m[nginx] config test failed, check /tmp/nginx-test.log\033[0m"
+  fi
+}
