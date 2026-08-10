8a2a718c751dcfbcfa143073cbc584da1e04e11a
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-10 21:48:04 +0700

    upload: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,23 @@ source /scripts/backup.sh
 source /scripts/web-terminal.sh
 source /scripts/tunnel.sh
 source /scripts/git-setup.sh
+source /scripts/nginx.sh
 
 setup_git_repo
 
 setup_runtime_paths
 detect_and_setup_runtime
 
+CHROME_BIN=$(find /opt/browsers -maxdepth 3 -type f -name "chrome" 2>/dev/null | head -n1)
+if [ -n "$CHROME_BIN" ]; then
+  export PUPPETEER_EXECUTABLE_PATH="$CHROME_BIN"
+fi
+
 start_web_terminal
 start_cf_tunnel
 start_auto_backup
 send_webhook_notification
+setup_nginx
 
 print_banner
 
