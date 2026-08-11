b21e20ce4cbcaf35090873ab0ae4fcd0ca536b21
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-11 18:46:37 +0700

    upload: scripts/identity.sh

--- scripts/identity.sh
+++ scripts/identity.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
 setup_identity() {
-  local uid gid uname
-  uid=$(id -u)
-  gid=$(id -g)
-  uname="${CONSOLE_USER:-container}"
-
-  if ! grep -q "^[^:]*:x:${uid}:" /etc/passwd 2>/dev/null; then
-    echo "${uname}:x:${uid}:${gid}:${uname}:/home/container:/bin/bash" >> /etc/passwd
-  fi
-
-  export USER="$uname"
-  export HOME=/home/container
-
-  local prompt='\[\e[1;32m\]'"${uname}"'\[\e[0m\]@\[\e[1;36m\]codex\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
-
-  {
-    echo "export PS1='${prompt}'"
-    echo "export TERM=xterm-256color"
-  } > /etc/profile.d/console-prompt.sh
-
-  if ! grep -q "console-prompt.sh" /etc/bash.bashrc 2>/dev/null; then
-    echo "source /etc/profile.d/console-prompt.sh" >> /etc/bash.bashrc
-  fi
+  export USER="root"
+  export HOME="/home/container"
+  export TERM="xterm-256color"
+  export PS1='\[\e[1;32m\]root\[\e[0m\]@\[\e[1;36m\]codex\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
 }
