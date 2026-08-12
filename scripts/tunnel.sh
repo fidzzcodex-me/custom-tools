b45c2bffc432fb46fd9b9b9a79197ec02196cdf0
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-12 18:45:16 +0700

    upload: scripts/tunnel.sh

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -3,7 +3,7 @@
 start_cf_tunnel() {
   [ "$ENABLE_CF_TUNNEL" != "true" ] && return 0
   if [ -z "$CF_TOKEN" ]; then
-    echo -e "\033[1;33m[tunnel] ENABLE_CF_TUNNEL is true but CF_TOKEN is empty, skipping.\033[0m"
+    echo -e "${C_YELLOW}[tunnel] ENABLE_CF_TUNNEL is true but CF_TOKEN is empty, skipping.${C_RESET}"
     return 0
   fi
   cloudflared tunnel run --token "$CF_TOKEN" >/tmp/cloudflared.log 2>&1 &
