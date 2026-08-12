0ce200dfea13f9c2159884d94fe3f995d65a5d76
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-12 18:45:24 +0700

    upload: scripts/identity.sh

--- scripts/identity.sh
+++ scripts/identity.sh
@@ -4,5 +4,9 @@ setup_identity() {
   export USER="root"
   export HOME="/home/container"
   export TERM="xterm-256color"
-  export PS1='\[\e[1;32m\]root\[\e[0m\]@\[\e[1;36m\]codex\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
+
+  local prompt='\[\033[1m\033[38;2;166;227;161m\]root\[\033[0m\]\[\033[38;2;108;112;134m\]@\[\033[0m\]\[\033[1m\033[38;2;137;220;235m\]codex\[\033[0m\]\[\033[38;2;108;112;134m\]:\[\033[0m\]\[\033[38;2;203;166;247m\]\w\[\033[0m\]\[\033[38;2;250;179;135m\]\$\[\033[0m\] '
+
+  export PS1="$prompt"
+  export CODEX_PS1="$prompt"
 }
