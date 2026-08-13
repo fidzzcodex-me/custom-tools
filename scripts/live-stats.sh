2adcb453b3a74e89abd3997bafb10b77656f8f87
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-13 11:56:51 +0700

    upload: scripts/live-stats.sh

--- scripts/live-stats.sh
+++ scripts/live-stats.sh
@@ -7,12 +7,17 @@ start_live_stats_ticker() {
   (
     while true; do
       sleep "$interval"
-      local uptime_str ram_str disk_used disk_total
+      local uptime_str ram_str disk_used disk_total mem_pct alert
       uptime_str=$(get_container_uptime)
       ram_str=$(get_container_memory)
       disk_used=$(df -h /home/container 2>/dev/null | awk 'NR==2 {print $3}')
       disk_total=$(df -h /home/container 2>/dev/null | awk 'NR==2 {print $2}')
-      echo -e "${C_OVERLAY}[${C_YELLOW}live${C_OVERLAY}]${C_RESET} ${C_TEXT}uptime=${C_TEAL}${uptime_str}${C_TEXT} ram=${C_TEAL}${ram_str}${C_TEXT} disk=${C_TEAL}${disk_used}/${disk_total}${C_RESET}"
+      mem_pct=$(get_memory_percent)
+      alert=""
+      if [ "$mem_pct" -ge 90 ] 2>/dev/null; then
+        alert=" ${C_RED}⚠ RAM ${mem_pct}%${C_RESET}"
+      fi
+      echo -e "${C_OVERLAY}[${C_YELLOW}live${C_OVERLAY}]${C_RESET} ${C_TEXT}uptime=${C_TEAL}${uptime_str}${C_TEXT} ram=${C_TEAL}${ram_str}${C_TEXT} disk=${C_TEAL}${disk_used}/${disk_total}${C_RESET}${alert}"
     done
   ) &
 }
